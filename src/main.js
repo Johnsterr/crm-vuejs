6acd56cf4593d28b0dcf29666a809f43a831148e
Kriogenik <evgpashko92@gmail.com>
2020-09-14 11:14:19 +0700

    Add firebase and initialize

--- src/main.js
+++ src/main.js
@@ -7,10 +7,31 @@ import store from './store'
 import dateFilter from '@/filters/date.filter'
 import 'materialize-css/dist/js/materialize.min'
 
+// Команда npm i firebase
+// Делаем импорт firebase
+import firebase from 'firebase/app'
+// Подключение модуля авторизации
+import 'firebase/auth'
+// Подключение модуля базы данных
+import 'firebase/database'
+
 Vue.config.productionTip = false
 
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
+
+// Firebase конфигурация от сайта firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyCWdDNI31_i4WEUwgU5aLhaDpaGt5lCO_Y',
+  authDomain: 'crm-on-vuejs.firebaseapp.com',
+  databaseURL: 'https://crm-on-vuejs.firebaseio.com',
+  projectId: 'crm-on-vuejs',
+  storageBucket: 'crm-on-vuejs.appspot.com',
+  messagingSenderId: '1043094530579',
+  appId: '1:1043094530579:web:9fd90f9ee72eddaf31ede6'
+}
+// Инициализация firebase
+firebase.initializeApp(firebaseConfig)
 
 new Vue({
   router,
