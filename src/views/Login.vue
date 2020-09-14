ab77b137e14d17fd25d624c0ab92d75e6aa032c6
Kriogenik <evgpashko92@gmail.com>
2020-09-14 17:12:30 +0700

    Add validate for Register form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,44 +4,52 @@
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
         <input
-            id="email"
-            type="text"
-            v-model.trim="email"
-            :class="{invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email)}"
+          id="email"
+          type="text"
+          v-model.trim="email"
+          :class="{invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email)}"
         >
         <label for="email">Email</label>
         <small
-        class="helper-text invalid"
-        v-if="$v.email.$dirty && !$v.email.required"
-        >Поле Email не должно быть пустым</small>
+          class="helper-text invalid"
+          v-if="$v.email.$dirty && !$v.email.required"
+        >
+          Поле Email не должно быть пустым
+        </small>
         <small
-        class="helper-text invalid"
-        v-else-if="$v.email.$dirty && !$v.email.email"
-        >Введите корректный Email</small>
+          class="helper-text invalid"
+          v-else-if="$v.email.$dirty && !$v.email.email"
+        >
+          Введите корректный Email
+        </small>
       </div>
       <div class="input-field">
         <input
-            id="password"
-            type="password"
-            v-model.trim="password"
-            :class="{invalid: ($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && !$v.password.minLength)}"
+          id="password"
+          type="password"
+          v-model.trim="password"
+          :class="{invalid: ($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && !$v.password.minLength)}"
         >
         <label for="password">Пароль</label>
         <small
-        class="helper-text invalid"
-        v-if="$v.password.$dirty && !$v.password.required"
-        >Введите пароль</small>
+          class="helper-text invalid"
+          v-if="$v.password.$dirty && !$v.password.required"
+        >
+          Введите пароль
+        </small>
         <small
-        class="helper-text invalid"
-        v-else-if="$v.password.$dirty && !$v.password.minLength"
-        >Пароль должен {{$v.password.$params.minLength.min}} символов. Сейчас он {{password.length}}</small>
+          class="helper-text invalid"
+          v-else-if="$v.password.$dirty && !$v.password.minLength"
+        >
+          Пароль должен {{$v.password.$params.minLength.min}} символов. Сейчас он {{password.length}}
+        </small>
       </div>
     </div>
     <div class="card-action">
       <div>
         <button
-            class="btn waves-effect waves-light auth-submit"
-            type="submit"
+          class="btn waves-effect waves-light auth-submit"
+          type="submit"
         >
           Войти
           <i class="material-icons right">send</i>
