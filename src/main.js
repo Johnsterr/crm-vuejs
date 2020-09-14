0af702c154fd88942eb51887ec2917aaae4918be
Kriogenik <evgpashko92@gmail.com>
2020-09-14 12:23:59 +0700

    Create auth module

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
+import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
 // Команда npm i firebase
